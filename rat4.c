1afdfdb27f3a69040e8bc58a494f8fa2402e82d7
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-08-15 23:14:34 +0000

    D: keywords.h:   * no more needed: it was used by rat4.c only
    M: rat4.c:
      * content of keywords.h moved in here
    
    M: Makefile:
      * do not make rat4.c depend on keywords.h anymore

--- rat4.c
+++ rat4.c
@@ -9,8 +9,56 @@
 #include "utils.h"
 #include "lookup.h"
 #include "error.h"
-#include "keywords.h"
 #include "rat4.h"
+
+/* keywords: */
+
+char sdo[] = "do";
+char vdo[] = { LEXDO, EOS };
+
+char sif[] = "if";
+char vif[] = { LEXIF, EOS };
+
+char selse[] = "else";
+char velse[] = { LEXELSE, EOS };
+
+#ifdef F77
+char sthen[] = "then";
+char sendif[] = "endif";
+#endif /* F77 */
+
+char swhile[] = "while";
+char vwhile[] = { LEXWHILE, EOS };
+
+char ssbreak[] = "break";
+char vbreak[] = { LEXBREAK, EOS };
+
+char snext[] = "next";
+char vnext[] = { LEXNEXT, EOS };
+
+char sfor[] = "for"; 
+char vfor[] = { LEXFOR, EOS };
+
+char srept[] = "repeat";
+char vrept[] = { LEXREPEAT, EOS };
+
+char suntil[] = "until";
+char vuntil[] = { LEXUNTIL, EOS };
+
+char sswitch[] = "switch";
+char vswitch[] = { LEXSWITCH, EOS };
+
+char scase[] = "case";
+char vcase[] = { LEXCASE, EOS };
+
+char sdefault[] = "default";
+char vdefault[] = { LEXDEFAULT, EOS };
+
+char sret[] = "return";
+char vret[] = { LEXRETURN, EOS };
+
+char sstr[] = "string";
+char vstr[] = { LEXSTRING, EOS };
 
 /* constant strings */
 
