0ff6ab1ed2a0d9db20596847080f839f6790a9a8
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-09-09 15:20:36 +0200

    refactoring (improved names of variables)
    M: src/*.[ch]:
      * Renamed global variable `leaveC' to `keep_comments': it's a clearer
        and more expressive name, and it's consistent with our hacking rule
        of not to have mixedCase names for variables.

--- src/rat4-global.h
+++ src/rat4-global.h
@@ -2,7 +2,7 @@
 #  define RAT4_GLOBAL_H 1
 #include "rat4-common.h"
 char fcname[MAXNAME];           /*  text of current function name                */
-int leaveC;                     /*  YES if comments should be kept, NO otherwise */
+int keep_comments;              /*  YES if comments should be kept, NO otherwise */
 int level;                      /*  level of file inclusion; init = 1            */
 int linect[NFILES];             /*  line count on input file[level]; init = 1    */
 FILE *infile[NFILES];           /*  file number[level]; init infile[1] = STDIN   */
