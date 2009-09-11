96d00d21ecdf1e4c0673993bef4cfae34b9d7256
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-09-11 03:38:14 +0200

    refactoring
    M: src/parser.c:
      * use switch statements were possible, instead of long if/elsif/else

--- src/parser.c
+++ src/parser.c
@@ -21,19 +21,22 @@ unstak(int *sp, int lextyp[], int labval[], char token)
     int tp;
 
     for (tp = *sp; tp > 0; tp--) {
-        if (lextyp[tp] == LBRACE)
+        switch(lextyp[tp])
+        {
+        case LBRACE:
+        case LEXSWITCH:
+            goto out;
             break;
-        if (lextyp[tp] == LEXSWITCH)
-            break;
-        if (lextyp[tp] == LEXIF && token == LEXELSE)
-            break;
-        if (lextyp[tp] == LEXIF)
+        case LEXIF:
+            if (token == LEXELSE)
+                goto out;
 #ifdef F77
             ifend();
 #else
             outcon(labval[tp]);
 #endif /* F77 */
-        else if (lextyp[tp] == LEXELSE) {
+            break;
+        case LEXELSE:
             if (*sp > 1)
                 tp--;
 #ifdef F77
@@ -41,16 +44,22 @@ unstak(int *sp, int lextyp[], int labval[], char token)
 #else
             outcon(labval[tp]+1);
 #endif /* F77 */
-        }
-        else if (lextyp[tp] == LEXDO)
+            break;
+        case LEXDO:
             dostat(labval[tp]);
-        else if (lextyp[tp] == LEXWHILE)
+            break;
+        case LEXWHILE:
             whiles(labval[tp]);
-        else if (lextyp[tp] == LEXFOR)
+            break;
+        case LEXFOR:
             fors(labval[tp]);
-        else if (lextyp[tp] == LEXREPEAT)
+            break;
+        case LEXREPEAT:
             untils(labval[tp], token);
-    }
+            break;
+        } /* switch */
+    } /* for */
+out:
     *sp = tp;
 }
 
@@ -86,19 +95,30 @@ parse(void)
     sp = 0;
     lextyp[0] = EOF;
     while ((token = lex(lexstr)) != EOF) {
-        if (token == LEXIF)
+        
+        /* do code generation */
+        switch(token)
+        {
+        case LEXIF:
             ifcode(&lab);
-        else if (token == LEXDO)
+            break;
+        case LEXDO:
             docode(&lab);
-        else if (token == LEXWHILE)
+            break;
+        case LEXWHILE:
             whilecode(&lab);
-        else if (token == LEXFOR)
+            break;
+        case LEXFOR:
             forcode(&lab);
-        else if (token == LEXREPEAT)
+            break;
+        case LEXREPEAT:
             repcode(&lab);
-        else if (token == LEXSWITCH)
+            break;
+        case LEXSWITCH:
             swcode(&lab);
-        else if (token == LEXCASE || token == LEXDEFAULT) {
+            break;
+        case LEXCASE:
+        case LEXDEFAULT:
             for (i = sp; i >= 0; i--)
                 if (lextyp[i] == LEXSWITCH)
                     break;
@@ -106,54 +126,75 @@ parse(void)
                 synerr("illegal case or default.");
             else
                 cascode(labval[i], token);
-        }
-        else if (token == LEXDIGITS)
+            break;
+        case LEXDIGITS:
             labelc(lexstr);
-        else if (token == LEXELSE) {
+            break;
+        case LEXELSE:
             if (lextyp[sp] == LEXIF)
                 elseifc(labval[sp]);
             else
                 synerr("illegal else.");
-        }
-        if (token == LEXIF || token == LEXELSE || token == LEXWHILE
-            || token == LEXFOR || token == LEXREPEAT
-            || token == LEXDO || token == LEXDIGITS
-            || token == LEXSWITCH || token == LBRACE)
+            break;
+        } /* switch token */
+        
+        /* manange stack of statements */
+        switch(token)
         {
+        case LEXIF:
+        case LEXELSE:
+        case LEXWHILE:
+        case LEXFOR:
+        case LEXREPEAT:
+        case LEXDO:
+        case LEXDIGITS:
+        case LEXSWITCH:
+        case LBRACE:
             sp++; /* beginning of statement */
             if (sp > MAXSTACK)
                 synerr_fatal("stack overflow in parser.");
             lextyp[sp] = token; /* stack type and value */
             labval[sp] = lab;
-        }
-        else if (token != LEXCASE && token != LEXDEFAULT) {
-            /*
-             * end of statement - prepare to unstack
-             */
-            if (token == RBRACE) {
-                if (lextyp[sp] == LBRACE)
-                    sp--;
-                else if (lextyp[sp] == LEXSWITCH) {
-                    swend(labval[sp--]);
-                }
-                else
-                    synerr("illegal right brace.");
+            break;
+        case LEXCASE:
+        case LEXDEFAULT:
+            /* do nothing */
+            break;
+        default:
+            /* end of statement - prepare to unstack */
+            switch(token)
+            {
+                case RBRACE:
+                    if (lextyp[sp] == LBRACE) {
+                        sp--;
+                    } else if (lextyp[sp] == LEXSWITCH) {
+                        swend(labval[sp--]);
+                    }
+                    else {
+                        synerr("illegal right brace.");
+                    }
+                    break;
+                case LEXOTHER:
+                    otherstmt(lexstr);
+                    break;
+                case LEXBREAK:
+                case LEXNEXT:
+                    brknxt(sp, lextyp, labval, token);
+                    break;
+                case LEXRETURN:
+                    retcode();
+                    break;
+                case LEXSTRING:
+                    strdcl();
+                    break;
             }
-            else if (token == LEXOTHER)
-                otherstmt(lexstr);
-            else if (token == LEXBREAK || token == LEXNEXT)
-                brknxt(sp, lextyp, labval, token);
-            else if (token == LEXRETURN)
-                retcode();
-            else if (token == LEXSTRING)
-                strdcl();
-            token = lex(lexstr);      /* peek at next token */
+            token = lex(lexstr); /* peek at next token */
             pbstr(lexstr);
             unstak(&sp, lextyp, labval, token);
         }
-    }
+    } /* while read next token */
     if (sp != 0)
-        synerr_eof();
+        synerr_eof(); /* XXX: more detailed error message? */
 }
 
 /* vim: set ft=c ts=4 sw=4 et : */
