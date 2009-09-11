50d9a96315c42e3bd9189a6e5cad36873aa8a082
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-09-11 16:05:24 +0200

    extending parser stack depth, new related tests
    M: src/parser.c:
      * stack depth increased to 1024 (was 100)
    
    A: tests/stmt-stack-stress_f77.test:
      * new test
    
    M:tests/Makefile.am:
      * $(ALL_TESTS) updated accordingly

--- src/parser.c
+++ src/parser.c
@@ -9,7 +9,7 @@
 #include "codegen.h" /* we need to set the starting label */
 #include "parser.h"
 
-#define MAXSTACK 100  /* max stack depth for parser XXX better value? */
+#define MAXSTACK 1024  /* max stack depth for parser */
 
 /*
  * P A R S E R
