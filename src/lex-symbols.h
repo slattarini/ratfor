1d733da8baf50f00b60a2e1f6db937924306c460
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-09-12 12:33:48 +0200

    disabled case folding for input
    Modern fortran compilers are case-insensible, but do not barf on mixed-case
    input, so the conversion to lowercase done by ratfor was useless.  Well, it
    was worse than that, since it caused some subtel problems.  So we disable
    the folding-to-lowercase functionality of ratfor.
    
    M: src/lex-symbols.h:
      * new lexical type `STRING'
    
    M: src/tokenizer.c:
      * in subroutine `deftok()': remove calls to `fold()' to convert the
        token read in to lowercase
      * in function `gtok()': return STRING if the token read is a string
    
    M: src/utils.c:
    M: src/utils.h:
      * function `fold()' removed
    
    A: tests/case-sensibility-basic.test:
    A: tests/include-no-uppercase-alias.test:
      * new tests
    
    M: tests/include-abspath.test:
      * slighty simplified
    
    M: tests/define-case-sensibility.test:
    M: tests/define-no-uppercase-alias.test:
    M: tests/define-nested.test:
    M: tests/define-multiline.test:
    M: tests/define-multiline-with-comment.test:
    M: tests/define-redefine.test:
    M: tests/include-basic.test:
    M: tests/include-nested.test:
    M: tests/include-nested-deep.test:
    M: tests/include-stress.test:
    M: tests/include-defined.test:
    M: tests/define-basic_f77.test:
       * updated: account for the ratfor's just acquired case-sensibility
    
    M: tests/defs.in:
      * removed now useless subroutines `tolower()', `toupper()' and `getstr()'
    
    M: tests/Makefile.am:
      * removed from $(XFAIL_TESTS) the following tests, which now work:
         - include-case-sensible.test
         - define-no-uppercase-alias.test
         - define-case-sensibility.test
      * added new tests `include-no-uppercase-alias.test' and
        `case-sensibility-basic.test' to $(ALL_TESTS)
    
    M: NEWS:
      * Tell that now ratfor is fully case-sensible w.r.t. both input and
        output, and relies on the fortran compiler being case-insensible
        (in the right way of course, e.g. the string constant must be
        case-sensible and the variables identifiers must not).
        This fixes an handful of long-standing bugs.
    
    M: BUGS:
      * Removed corrected bugs (in this or previous checkins):
         - "the `include' statement coerce the name of the file to include
            to lowercase"
         - "the builtin `define' is case-insensible w.r.t. symbol names"
         - "lines starting with `%'  should be copied verbatim, but are are
            still subjected to line wrapping"

--- src/lex-symbols.h
+++ src/lex-symbols.h
@@ -14,25 +14,26 @@
 #define LETTER          1
 #define DIGIT           2
 #define ALPHA           3
+#define STRING          4
 
-#define LEXBREAK        4
-#define LEXDIGITS       5
-#define LEXDO           6
-#define LEXELSE         7
-#define LEXFOR          8
-#define LEXIF           9
-#define LEXNEXT         10
-#define LEXOTHER        11
-#define LEXREPEAT       12
-#define LEXUNTIL        13
-#define LEXWHILE        14
-#define LEXRETURN       15
-#define LEXEND          16
-#define LEXSTOP         17
-#define LEXSTRING       18
-#define LEXSWITCH       19
-#define LEXCASE         20
-#define LEXDEFAULT      21
+#define LEXBREAK        10
+#define LEXDIGITS       11
+#define LEXDO           12
+#define LEXELSE         13
+#define LEXFOR          14
+#define LEXIF           15
+#define LEXNEXT         16
+#define LEXOTHER        17
+#define LEXREPEAT       18
+#define LEXUNTIL        19
+#define LEXWHILE        20
+#define LEXRETURN       21
+#define LEXEND          22
+#define LEXSTOP         23
+#define LEXSTRING       24
+#define LEXSWITCH       25
+#define LEXCASE         26
+#define LEXDEFAULT      27
 
 #endif
 /* vim: set ft=c ts=4 sw=4 et : */
--- src/utils.h
+++ src/utils.h
@@ -3,7 +3,6 @@
 char *strsave(const char *);
 int ctoi(const char[], int *);
 int itoc(int, char[], int size);
-void fold(char[]);
 void scopy(const char[], int, char[], int);
 int look(const char[], char[]);
 #endif /* RAT4_UTILS_H */
