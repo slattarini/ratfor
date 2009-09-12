83b1a2bf36f262d9865cd362dcf8d1f0e9a76dab
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-09-12 17:01:53 +0200

    ratfor: deal better with output file (`-o' option)
    M: src/main.c:
      * when option `-o' is read by the ratfor option parse, do not try to
        immediately open the given  file for writing, just save its name to
        open it later; this way, if ratfor is called as e.g.:
          ratfor -o OUTFILE1 -o OUTFILE2
        output is written to OUTFILE2, without unnecessarily clobbering
        the content of OUTFILE1.
      * improved error messages for `-o': e.g. when outfile is a directory,
        do not simply fail with the message:
          "outfile: cannot open for writing"
        but with the more precise message:
          "outfile: cannot open for writing: Is a directory"
      * `-o -' now tell ratfor to write outout to standard output
    
    M: src/error.h:
      * include `errno.h' (for `errno' variable) and `string.h' (for
        `strerror()' function) unconditionally
    
    M: NEWS:
      * updated accordingly
    
    A: tests/outfile-cantcreate-fail.test:
    A: tests/outfile-dash-is-stdout.test:
    A: tests/outfile-devnull.test:
    A: tests/outfile-directory-fail.test:
    A: tests/outfile-no-multi-clobber.test:
    A: tests/outfile-overwrite.test:
    A: tests/outfile-no-useless-overwrite.test:
    A: tests/outfile-unwritable-fail.test:
    A: tests/outfile.test:
      * new tests
    
    M: tests/Makefile.am:
      * $(ALL_TESTS) updated accordingly

--- src/error.h
+++ src/error.h
@@ -1,10 +1,16 @@
 #ifndef RAT4_ERROR_H 
 #  define RAT4_ERROR_H 1
+
+#include <errno.h>  /* for the `errno' variable */
+#include <string.h> /* for `strerror()' function */
+
+extern int exit_status;
+
 void fatal(const char *, ...);
 void synerr(const char *, ...);
 void synerr_eof(void);
 void synerr_include(const char *, ...);
 void synerr_fatal(const char *, ...);
-extern int exit_status;
+
 #endif
 /* vim: set ft=c ts=4 sw=4 et : */
