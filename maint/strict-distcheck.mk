a4e287798d5d7fedddd0e1e27e80ffea522fe69a
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-09-25 12:15:57 +0200

    maint make rules in GNUmakefile made optional
    Now we let the user decide if he wants to import maintainer make
    rules in GNUmakefile, through the new automake conditional
    `RAT4_ENABLE_MAINTAINER_MAKE_RULES' and new configure option
    `--enable-maintainer-make-rules'.
    
    M: Makefile.am:
      * define macro `$(RAT4_ENABLE_MAINTAINER_RULES)' to `yes' if the
        automake conditional `RAT4_ENABLE_MAINTAINER_MAKE_RULES' is true,
        to `no' otherwise
    
    M: maint.mk:
      * splitted in some new files maint/*.mk (listed below)
      * consider rules and definition of iterest rules
      * includes the maint/*.mk files only if the macro
        `$(RAT4_ENABLE_MAINTAINER_MAKE_RULES)' is set to "yes"
        (which happens only if the automake conditional
        `RAT4_ENABLE_MAINTAINER_MAKE_RULES' is true),
    
    A: maint/strict-distcheck.mk:
      * new file, from older `maint.mk', contains maintainer rules and
        definitions to run `strict-distcheck'
      * removed support to automatically bootstrap source tree with make
        from an unconfigured source tree
    
    A: maint/release.mk:
      * new file, from older `maint.mk', contains maintainer rules to build
        a beta or stable release
    
    A: maint/remake.mk:
      * new file, from older `maint.mk', contains maintainer rules to
        regenerate autotools files and properly reconfigure the source/build
        tree
    
    M: Makefile.am:
      * added the new maint/*.mk files to `$(EXTRA_DIST)'
    
    M: NEWS:
      * updated accordingly, by telling that now maintainer-specific make rules
        are imported only if the build tree has been configured with the
        `--enable-maintainer-make-rules' option
      * nitpick: fixed spacing after full stops

---
+++ maint/strict-distcheck.mk
@@ -0,0 +1,123 @@
+# -*- Makefile -*-
+# Contains maintainer-specific rules to run target `strict-distcheck',
+# and "on-steroids" version of distcheck. Included by top-level mait.mk.
+
+ME := strict-distcheck.mk
+
+# Use this to make sure we don't run these programs when building from a
+# virgin tgz file in `strict-distcheck'.
+strict_distcheck_null_am_makeflags ?= \
+  ACLOCAL=false \
+  AUTOCONF=false \
+  AUTOMAKE=false \
+  AUTOHEADER=false \
+  MAKEINFO=false
+
+# Command line arguments for configure as called by `strict-distcheck'.
+# Will be updated later.
+strict_distcheck_configure_flags =
+
+# Auxilary variables, needed to keep the code in target `strict-distcheck'
+# understandable. Will be suitably updated later, in various places.
+StrictDistcheckLoopBegin =
+StrictDistcheckLoopEnd =
+
+# C/C++ compilers to be used to by the ratfor testsuite, when doing a
+# strict-distcheck.
+strict_distcheck_c_compilers ?= cc c++
+StrictDistcheckLoopBegin += \
+  for cc in $(strict_distcheck_c_compilers); do
+StrictDistcheckLoopEnd += done;
+strict_distcheck_configure_flags += CC='$$cc'
+strict_distcheck_configure_flags += --enable-werror-cflag
+
+# Fortran compilers to be used by the ratfor testsuite, when doing a
+# strict-distcheck.
+strict_distcheck_f77_compilers ?= f77 gfortran
+StrictDistcheckLoopBegin += \
+  for f77 in NONE $(strict_distcheck_f77_compilers); do
+StrictDistcheckLoopEnd += done;
+strict_distcheck_configure_flags += F77='$$f77'
+
+# Shells to be used to run the configure script and the tests scripts,
+# when doing a strict-distcheck.  Since the rules in `make distcheck'
+# run `configure' directly, we need to go through some hoops to force
+# it to run with the shell we want.
+strict_distcheck_shells ?= ksh bash
+StrictDistcheckLoopBegin += \
+  for sh_t in /bin/sh $(strict_distcheck_shells); do \
+    case "$$sh_t" in \
+      *'[ 	]'*) \
+        sh=`(set $$sh_t && shift && echo $$1)`; \
+        sh_args=`(set $$sh_t && shift && shift && echo $$*)`;; \
+      *) \
+        sh=$$sh_t; sh_args='';; \
+    esac; \
+    case "$$sh" in \
+      /*) ;; \
+      */*) sh=`pwd`/$$sh;; \
+      *) sh=`which "$$sh" 2>/dev/null`;; \
+    esac; \
+    case "$$sh" in /*);; *) continue;; esac; \
+    test -f "$$sh" && test -x "$$sh" || continue; \
+    sh="$$sh $$sh_args"; \
+    _RAT4_STRICT_DISTCHECK_CONFIG_SHELL="$$sh"; \
+    export _RAT4_STRICT_DISTCHECK_CONFIG_SHELL; \
+    :
+StrictDistcheckLoopEnd += \
+    _RAT4_STRICT_DISTCHECK_CONFIG_SHELL=''; \
+    unset _RAT4_STRICT_DISTCHECK_CONFIG_SHELL || :; \
+  done;
+strict_distcheck_configure_flags += CONFIG_SHELL='$$sh'
+# Since the rules in `make distcheck' run `configure' directly, we need
+# to go through some hoops to force it to run with the shell we want.
+.PHONY: config-shell-strict-distcheck-hook
+distcheck-hook: config-shell-strict-distcheck-hook
+config-shell-strict-distcheck-hook:
+	@test -z "$${_RAT4_STRICT_DISTCHECK_CONFIG_SHELL-}" || { \
+	  echo "$(ME): Correcting configure script for strict-distcheck" \
+	  && tmp=configure.tmp \
+	  && sh=$${_RAT4_STRICT_DISTCHECK_CONFIG_SHELL} \
+	  && rm -f $$tmp \
+	  && echo "#! $$sh" >>$$tmp \
+	  && echo "# Correction code added by $(ME)" >>$$tmp \
+	  && echo "CONFIG_SHELL='$$sh'" >>$$tmp \
+	  && echo "export CONFIG_SHELL" >>$$tmp \
+	  && sed '1s/^#!.*$$//' $(distdir)/configure >>$$tmp \
+	  && chmod u+w $(distdir)/configure \
+	  && cat $$tmp >$(distdir)/configure \
+	  && chmod a-w $(distdir)/configure \
+	  && echo '|----------------' \
+	  && sed -e 's/^/| /' -e 40q $(distdir)/configure \
+	  && echo '|----------------' \
+	  && rm -f $$tmp; \
+	}
+
+.PHONY: strict-distcheck
+strict-distcheck: all check distcheck
+	set -e; \
+	 xrun() { \
+	   echo "+ $$*"; \
+	   "$$@" || { \
+	     echo "========================================================"; \
+	     echo "$(ME): COMMAND FAILED: $$*"; \
+	     echo "========================================================"; \
+	     exit 1; \
+	   }; \
+	 }; \
+	 echo " -*-*-*-"; \
+	 echo "$(ME): running $@"; \
+	 $(StrictDistcheckLoopBegin) \
+	   echo " -*-*-*-"; \
+	   xrun $(xmake) distcheck \
+	     AM_MAKEFLAGS='$(strict_distcheck_null_am_makeflags)' \
+	     DISTCHECK_CONFIGURE_FLAGS="$(strict_distcheck_configure_flags)"; \
+	   echo " -*-*-*- "; \
+	   xrun $(xmake) -j8 distcheck \
+	     AM_MAKEFLAGS='$(strict_distcheck_null_am_makeflags)' \
+	     DISTCHECK_CONFIGURE_FLAGS="$(strict_distcheck_configure_flags)"; \
+	 $(StrictDistcheckLoopEnd) \
+	 echo " -*-*-*-"; \
+	 exit $$e;
+
+# vim: ft=make ts=4 sw=4 noet
