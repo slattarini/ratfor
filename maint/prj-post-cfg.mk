55ff7591113ad71cb02d33a7f7d178646d01ff84
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-10-04 23:55:33 +0200

    strict-distcheck: adapt to latest SteLib's changes
    M: maint/prj-post-cfg.mk:
      * since SteLib's `strict-distcheck.mk' does not use/define C compilers
        anymore, we do this by ourselves
    
    M: maint/strict-distcheck.mk:
      * updated from latest version on SteLib (which does not use/define C
        compilers anymore)

--- maint/prj-post-cfg.mk
+++ maint/prj-post-cfg.mk
@@ -3,7 +3,19 @@
 # maintainer-specific targets
 
 #
-# Make compile-time warnings fatal also when checkin dist tarball
+# C compilers to be used to build ratfor, when doing a strict-distcheck.
+#
+
+# can be overridden by local-cfg.mk
+strict_distcheck_c_compilers ?= cc c++
+
+StrictDistcheckLoopBegin += \
+  for cc in $(strict_distcheck_c_compilers); do
+StrictDistcheckLoopEnd += done;
+strict_distcheck_configure_flags += CC='$$cc'
+
+#
+# Make C compile-time warnings fatal also when checkin dist tarball
 # with `strict-distcheck'
 #
 strict_distcheck_configure_flags += --enable-werror-cflag
