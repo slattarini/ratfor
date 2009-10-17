5971db1b1c2d9ad41651dbde287a42d0a9ca61dd
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-10-17 13:34:15 +0200

    bootstrap.sh: let the user avoid autoreconf call
    M: maint/bootstrap.sh:
      * accept options `--no-autoreconf' (to skip the call to autoreconf) and
        `--autoreconf' (to force the call to autoreconf)
      * bailout on unrecognized options

--- maint/bootstrap.sh
+++ maint/bootstrap.sh
@@ -1,17 +1,40 @@
 #!/bin/sh
 # Bootstrap the Ratfor Testsuite package from checked-out sources.
 
-set -x
+USAGE="Usage: $0 [--autoreconf|--no-autoreconf]"
+
+vrun() {
+    echo "+ $*" >&2
+    "$@"
+}
+
+run_autoreconf=true
+while test $# -gt 0; do
+    case "$1" in
+        --help) echo "$USAGE"; exit;;
+        --autoreconf) run_autoreconf=true;;
+        --no-autoreconf) run_autoreconf=false;;
+        *) echo "$0: \`$1': Invalid argument" >&2
+           echo "$USAGE" >&2
+           exit 2;;
+    esac
+    shift
+done
+
 set -e
 
 AUTORECONF=${AUTORECONF-"autoreconf"}
 
 f=tests/TestsList.am
-rm -f $f $f-t
-sh ./build-aux/gen-tests-list.sh tests >$f-t
-chmod a-w $f-t
-mv $f-t $f
+vrun rm -f $f $f-t
+vrun sh ./build-aux/gen-tests-list.sh tests >$f-t
+vrun chmod a-w $f-t
+vrun mv $f-t $f
 
-$AUTORECONF -vi
+if $run_autoreconf; then
+    vrun $AUTORECONF -vi
+else
+    :
+fi
 
 # vim: ft=sh ts=4 sw=4 et
