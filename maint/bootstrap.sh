9abef32a2d9c12d9f57c662e4c8ca1a095a7b9de
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-10-17 15:08:15 +0200

    updated blackbox-tests (autogenerated tests list)
    M: blackbox-tests:
      * submodule updated to a version which automatically generates the list
        of test scripts
    
    M: maint/bootstrap.sh:
      * modified to appropriately call the bootstrap script of the
        `blackbox-tests' submodule

--- maint/bootstrap.sh
+++ maint/bootstrap.sh
@@ -7,9 +7,16 @@ AUTORECONF=${AUTORECONF-"autoreconf"}
 set -e
 set -x
 
+pwd
 $GIT submodule init
 $GIT submodule update
 
+cd blackbox-tests
+pwd
+./bootstrap.sh --no-autoreconf
+cd ..
+
+pwd
 $AUTORECONF -vi
 
 # vim: ft=sh ts=4 sw=4 et
