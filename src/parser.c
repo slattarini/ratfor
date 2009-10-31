56bbbb368e155fd636fb6af5883d9173aee5982c
Stefano Lattarini <stefano.lattarini@gmail.com>
2009-10-31 11:53:46 +0100

    parser.c: normalized indentation of switch statements
    M: src/parser.c:
      * unstack(), parse(): normalized indentation of switch statements:
        indent also the `case' labels

--- src/parser.c
+++ src/parser.c
@@ -47,34 +47,33 @@ static void
 unstak(char token)
 {
     for (; sp > 0; sp--) {
-        switch(lextyp[sp])
-        {
-        case LBRACE:
-        case LEXSWITCH:
-            return;
-            break;
-        case LEXIF:
-            if (token == LEXELSE)
+        switch(lextyp[sp]) {
+            case LBRACE:
+            case LEXSWITCH:
                 return;
-            ifend();
-            break;
-        case LEXELSE:
-            if (sp > 1)
-                sp--;
-            ifend();
-            break;
-        case LEXDO:
-            dostat(labval[sp]);
-            break;
-        case LEXWHILE:
-            whiles(labval[sp]);
-            break;
-        case LEXFOR:
-            fors(labval[sp]);
-            break;
-        case LEXREPEAT:
-            untils(labval[sp], token);
-            break;
+                break;
+            case LEXIF:
+                if (token == LEXELSE)
+                    return;
+                ifend();
+                break;
+            case LEXELSE:
+                if (sp > 1)
+                    sp--;
+                ifend();
+                break;
+            case LEXDO:
+                dostat(labval[sp]);
+                break;
+            case LEXWHILE:
+                whiles(labval[sp]);
+                break;
+            case LEXFOR:
+                fors(labval[sp]);
+                break;
+            case LEXREPEAT:
+                untils(labval[sp], token);
+                break;
         } /* end switch */
     } /* end for */
 }
@@ -94,97 +93,95 @@ parse(void)
             continue;
 
         /* do code generation */
-        switch(token)
-        {
-        case LEXIF:
-            ifcode(&lab);
-            break;
-        case LEXDO:
-            docode(&lab);
-            break;
-        case LEXWHILE:
-            whilecode(&lab);
-            break;
-        case LEXFOR:
-            forcode(&lab);
-            break;
-        case LEXREPEAT:
-            repcode(&lab);
-            break;
-        case LEXSWITCH:
-            swcode(&lab);
-            break;
-        case LEXCASE:
-        case LEXDEFAULT:
-            for (i = sp; i >= 0; i--)
-                if (lextyp[i] == LEXSWITCH)
-                    break;
-            if (i < 0)
-                synerr("illegal case or default.");
-            else
-                cascode(labval[i], token);
-            break;
-        case LEXDIGITS:
-            labelc(lexstr);
-            break;
-        case LEXELSE:
-            if (lextyp[sp] == LEXIF)
-                elseifc();
-            else
-                synerr("illegal else.");
-            break;
+        switch(token) {
+            case LEXIF:
+                ifcode(&lab);
+                break;
+            case LEXDO:
+                docode(&lab);
+                break;
+            case LEXWHILE:
+                whilecode(&lab);
+                break;
+            case LEXFOR:
+                forcode(&lab);
+                break;
+            case LEXREPEAT:
+                repcode(&lab);
+                break;
+            case LEXSWITCH:
+                swcode(&lab);
+                break;
+            case LEXCASE:
+            case LEXDEFAULT:
+                for (i = sp; i >= 0; i--)
+                    if (lextyp[i] == LEXSWITCH)
+                        break;
+                if (i < 0)
+                    /* XXX: better error message */
+                    synerr("illegal case or default.");
+                else
+                    cascode(labval[i], token);
+                break;
+            case LEXDIGITS:
+                labelc(lexstr);
+                break;
+            case LEXELSE:
+                if (lextyp[sp] == LEXIF)
+                    elseifc();
+                else
+                    synerr("illegal else.");
+                break;
         } /* switch token */
         
         /* manange stack of statements */
-        switch(token)
-        {
-        case LEXIF:
-        case LEXELSE:
-        case LEXWHILE:
-        case LEXFOR:
-        case LEXREPEAT:
-        case LEXDO:
-        case LEXDIGITS:
-        case LEXSWITCH:
-        case LBRACE:
-            sp++; /* beginning of statement */
-            if (sp > MAXSTACK)
-                synerr_fatal("stack overflow in parser.");
-            lextyp[sp] = token; /* stack type and value */
-            labval[sp] = lab;
-            break;
-        case LEXCASE:
-        case LEXDEFAULT:
-            /* do nothing */
-            break;
-        default:
-            /* end of statement - prepare to unstack */
-            switch(token)
-            {
-                case RBRACE:
-                    if (lextyp[sp] == LBRACE) {
-                        sp--;
-                    } else if (lextyp[sp] == LEXSWITCH) {
-                        swend(labval[sp--]);
-                    }
-                    else {
-                        synerr("illegal right brace.");
-                    }
-                    break;
-                case LEXOTHER:
-                    otherstmt(lexstr);
-                    break;
-                case LEXBREAK:
-                case LEXNEXT:
-                    brknxt(sp, lextyp, labval, token);
-                    break;
-                case LEXRETURN:
-                    retcode();
-                    break;
-            }
-            token = lex(lexstr); /* peek at next token */
-            put_back_string(lexstr);
-            unstak(token);
+        switch(token) {
+            case LEXIF:
+            case LEXELSE:
+            case LEXWHILE:
+            case LEXFOR:
+            case LEXREPEAT:
+            case LEXDO:
+            case LEXDIGITS:
+            case LEXSWITCH:
+            case LBRACE:
+                sp++; /* beginning of statement */
+                if (sp > MAXSTACK)
+                    synerr_fatal("stack overflow in parser.");
+                lextyp[sp] = token; /* stack type and value */
+                labval[sp] = lab;
+                break;
+            case LEXCASE:
+            case LEXDEFAULT:
+                /* do nothing */
+                break;
+            default:
+                /* end of statement - prepare to unstack */
+                switch(token) {
+                    case RBRACE:
+                        if (lextyp[sp] == LBRACE) {
+                            sp--;
+                        } else if (lextyp[sp] == LEXSWITCH) {
+                            swend(labval[sp--]);
+                        }
+                        else {
+                            synerr("illegal right brace.");
+                        }
+                        break;
+                    case LEXOTHER:
+                        otherstmt(lexstr);
+                        break;
+                    case LEXBREAK:
+                    case LEXNEXT:
+                        brknxt(sp, lextyp, labval, token);
+                        break;
+                    case LEXRETURN:
+                        retcode();
+                        break;
+                }
+                token = lex(lexstr); /* peek at next token */
+                put_back_string(lexstr);
+                unstak(token);
         }
     } /* while read next token */
     if (sp != 0)
