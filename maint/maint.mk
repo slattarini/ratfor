6a511bc3573b1861db7788971fbb7cc011a28c59
Stefano Lattarini <stefano.lattarini@gmail.com>
2010-01-24 17:15:33 +0100

    Removed `strict-distcheck' functionality (too much hackish and fragile)
    * maint/maint.mk: Updated from SteLib. This version does not try to
    include the `strict-distcheck.mk' file anymore.
    * maint/strict-distcheck.mk: Removed.
    * maint/personal/stefano-lattarini.mk: Removed, as it has become
    obsoleted.
    * maint/prj-post-cfg.mk: Likewise.
    * Makefile.am (EXTRA_DIST): Removed deleted files.
    * HACKING (Making a release): Do not cite `strict-distcheck'
    anymore.  To compensate, describe an alternative strategy that
    can be used to do "stronger" testing, which is advised before
    a release.

--- maint/maint.mk
+++ maint/maint.mk
@@ -1,6 +1,24 @@
 # -*- Makefile -*-
-# Copied from SteLib at 2009-11-13 16:50:07 +0100.  DO NOT EDIT!
-# Contains maintainer-specific rules. Included by top-level GNUmakefile.
+# Copied from SteLib at 2010-01-24 16:41:54 +0100.  DO NOT EDIT!
+#
+#  Contains maintainer-specific rules. Included by top-level GNUmakefile.
+#
+#  Copyright (C) 2009, 2010 Stefano Lattarini.
+#  Written by Stefano Lattarini <stefano.lattarini@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 # Do not save the original name or timestamp in the .tar.gz file.
 # Use --rsyncable if available.
@@ -68,7 +86,6 @@ endef
 # and modularity.
 #
 include $(maintdir)/developer.mk
-include $(maintdir)/strict-distcheck.mk
 include $(maintdir)/release.mk
 
 #
