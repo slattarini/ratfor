5a3b23a10495627edfc356a6c342c0f6a55b01e5
Stefano Lattarini <stefano.lattarini@gmail.com>
2010-02-22 17:06:08 +0100

    Ratfor control structures after numeric label: check their use (with f77)
    * tests/if-after-numeric-label_f77.test: New test.  Checks that
    the ratfor `if' statement is recognized after a numeric label.
    * tests/ifelse-after-numeric-label_f77.test: New test.  Checks
    that the ratfor `if/else' statement is recognized after a numeric
    label.
    * tests/while-after-numeric-label_f77.test: New test.  Checks
    that the ratfor `while' statement is recognized after a numeric
    label.
    * tests/for-after-numeric-label_f77.test: New test.  Checks that
    the ratfor `for' statement is recognized after a numeric label.
    * tests/repeat-after-numeric-label_f77.test: New test.  Checks
    that the ratfor `repeat' statement is recognized after a numeric
    label.
    * tests/repeatuntil-after-numeric-label_f77.test: New test.  Checks
    that the ratfor `repeat/until' statement is recognized after a
    numeric label.
    * tests/do-after-numeric-label_f77.test: New test.  Checks that
    the ratfor `do' statement is recognized after a numeric label.
    * tests/control-structure-after-numeric-label.sh: Helper script
    used by the previously listed new tests.
    * tests/Makefile.am (TESTS): Updated accordingly.
    (EXTRA_DIST): Added `control-structure-after-numeric-label_f77.sh'.
    (repeatuntil-after-numeric-label_f77.log): Depends from file
    `control-structure-after-numeric-label_f77.sh'.
    (repeat-after-numeric-label_f77.log): Likewise.
    (ifelse-after-numeric-label_f77.log): Likewise.
    (if-after-numeric-label_f77.log): Likewise.
    (while-after-numeric-label_f77.log): Likewise.
    (for-after-numeric-label_f77.log): Likewise.
    (do-after-numeric-label_f77.log): Likewise.

---
+++ tests/control-structure-after-numeric-label_f77.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+# Copyright (C) 2010 Stefano Lattarini <stefano.lattarini@gmail.com>.
+# This file is part of the Ratfor Testsuite.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Helper file, sourced by scripts checking (through Fortran) that ratfor
+# control statements (if, while, for, etc) are processed correctly when
+# found after a numerical label.  This file is expected to be sourced by
+# test scripts after the file `rat4-testsuite-init.sh' has already been
+# sourced.
+
+set +x
+echo "$me: INFO: disable shell verbosity while defining shell function"
+
+cat >exp <<'EOF'
+1
+2
+3
+EOF
+
+# Usage: write_test_file LOOP-OPENING-CODE LOOP-CLOSING-CODE
+write_test_file() {
+  cat > tst.r <<EOF
+program test
+    implicit none
+    integer i
+    i = 1
+    goto 10
+    write(*,100) i-1
+10  $1
+      write(*,100) i
+    $2
+20  write(*,100) i
+30  call halt
+100 format(I1)
+end
+EOF
+}
+
+do_test() {
+  write_test_file "$@"
+  cat tst.r
+  run_RATFOR tst.r || testcase_FAIL "unexpected ratfor failure"
+  test ! -s stderr || testcase_FAIL "ratfor produced diagnostic on stderr"
+  mv stdout tst.f
+  mv stderr ratfor-stderr
+  run_F77 tst.f || testcase_FAIL "ratfor produced uncompilable code"
+  run_command ./tst.exe || testcase_FAIL "testprg failed"
+  test ! -s stderr || testcase_FAIL "testprg produced diagnostic on stderr"
+  mv stdout got
+  cat exp
+  cat got
+  $DIFF_U exp got || testcase_FAIL "expected output and got output differs"
+}
+
+echo "$me: INFO: reactivate shell verbosity before running tests"
+set -x
+
+# vim: ft=sh ts=4 sw=4 et
